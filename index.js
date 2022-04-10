4f1015b671c228e27671b6a4b2723423d474f687
Sneha Mahajan <sneh.km@gmail.com>
2022-04-10 18:01:46 -0400

    Recursive array function implemented

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum =0;
+  if(!Array.isArray(array)){
+    return array;
+  }
+  for(const i of array){
+      sum += sumItems(i);
+  }
+  return sum;
 }
 
+//console.log(sumItems([1,1,[2,8,8],3]));
 module.exports = sumItems;
